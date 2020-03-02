3c91804b0a49c93f1da01cf9b80aa704dcaaae2c
igoldin74 <33078752+igoldin74@users.noreply.github.com>
2020-03-01 21:04:09 -0600

    Extracted methods in CreateNewGroupTests Created classes for account data and group data.

---
+++ GroupData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace addressbook_tests
+{
+    public class GroupData
+    {
+        // These are fields:
+        private string name;
+        private string header;
+        private string footer;
+
+        // This is constructor:
+        public GroupData(string name)
+        {
+            this.name = name;
+        }
+
+        // These are properties:
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Header
+        {
+            get
+            {
+                return header;
+            }
+            set
+            {
+                header = value;
+            }
+        }
+
+        public string Footer
+        {
+            get
+            {
+                return footer;
+            }
+            set
+            {
+                footer = value;
+            }
+        }
+    }
+}

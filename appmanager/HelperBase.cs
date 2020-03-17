80dbeea4e349a93ad8630a947a50884b36095275
igoldin74 <33078752+igoldin74@users.noreply.github.com>
2020-03-16 21:39:49 -0500

    Lecture 3.1: Implemented HelperBase method Type() with logic to check input string for null. If null don't type. Changed all modification methods and login method to use Type().

--- appmanager/HelperBase.cs
+++ appmanager/HelperBase.cs
@@ -12,5 +12,14 @@ namespace addressbook_tests
             this.manager = manager;
             this.driver = manager.Driver;
         }
+
+        public void Type(By locator, string text)
+        {
+            if (text != null)
+            {
+                driver.FindElement(locator).Clear();
+                driver.FindElement(locator).SendKeys(text);
+            }
+        }
     }
 }

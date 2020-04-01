8a57120881fb7a64c841a8e6be5bfafc21d8b53d
Eli Goldin <igoldin74@gmail.com>
2020-03-31 20:07:52 -0500

    Added ContactInformationTests

---
+++ tests/ContactInformationTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace addressbook_tests.tests
+{
+    [TestFixture]
+    public class ContactInformationTests : AuthTestBase
+    {
+        [Test]
+        public void TestContactInformationFromForm()
+        {
+            ContactData fromTable = app.ContactHelper.GetContactInformationFromTable(4);
+            ContactData fromForm = app.ContactHelper.GetContactInformationFromEditForm(4);
+            Assert.AreEqual(fromTable, fromForm);
+            Assert.AreEqual(fromTable.Address, fromForm.Address);
+            Assert.AreEqual(fromTable.AllPhones, fromForm.AllPhones);
+        }
+
+        [Test]
+        public void TestContactInformationFromViewPage()
+        {
+            ContactData fromTable = app.ContactHelper.GetContactInformationFromTable(0);
+            ContactData fromViewPage = app.ContactHelper.GetContactInformationFromViewPage(0);
+            Assert.AreEqual(fromTable, fromViewPage);
+        }
+    }
+}
